528d0a9a38f34ce716765e496e1c547fd52b04af
katchen1 <katherinechen139@gmail.com>
2023-06-04 07:18:45 -0700

    Initial Commit

---
+++ EmoRecApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmoRecApp
+//
+//  Created by Katherine Chen on 6/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmoRecApp/EmoRecAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmoRecAppApp.swift
+//  EmoRecApp
+//
+//  Created by Katherine Chen on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct EmoRecAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
